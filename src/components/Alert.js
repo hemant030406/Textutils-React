d93032cf971b27f7d25c980f680a3d6f8abff68e
HEMANT PATHAK <122811374+hemant030406@users.noreply.github.com>
2024-03-01 22:03:10 +0530

    more readable now

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export default function Alert(props) {
     return (
         props.alert &&
         <div className={`alert alert-${props.alert.type.toLowerCase()}`} role="alert">
-        <strong>{props.alert.type}:</strong>{props.alert.msg}
+            <strong>{props.alert.type}:</strong>{props.alert.msg}
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,39 +5,39 @@ import {
 } from "react-router-dom";
 
 export default function Navbar(props) {
-    return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="#">{props.title}</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">{props.AboutText}</Link>
-        </li>
-      </ul>
-      {/* <button type="button" className="btn btn-secondary mx-2">Grey</button>
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="#">{props.title}</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">{props.AboutText}</Link>
+            </li>
+          </ul>
+          {/* <button type="button" className="btn btn-secondary mx-2">Grey</button>
       <button type="button" className="btn btn-success mx-2">Green</button>
       <button type="button" className="btn btn-danger mx-2">Red</button>
       <button type="button" className="btn btn-warning mx-2">Yellow</button>
       <button type="button" className="btn btn-info mx-2">Sky Blue</button> */}
 
-        <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-          <input className="form-check-input" type="checkbox" role="switch"           id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.btnText} </label>
+          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.btnText} </label>
+          </div>
+          <form className="d-flex  mx-4" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-primary" type="submit">Search</button>
+          </form>
         </div>
-      <form className="d-flex  mx-4" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-primary" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
